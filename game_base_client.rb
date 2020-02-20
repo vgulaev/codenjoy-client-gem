16512e44f8f815292ece823caae2c545375542be
vgulaev <vgulaev@yandex.ru>
2020-02-20 14:33:00 +0500

    Up

--- game_base_client.rb
+++ game_base_client.rb
@@ -7,7 +7,7 @@ game = Codenjoy::Client::Game.new
 url = "https://dojorena.io/codenjoy-contest/board/player/70xewv6o7ddy9yphm1u0?code=2603484461919438773&gameName=battlecity"
 count = 0
 
-game.play(url, 'debug') do |ws, msg|
+game.play(url) do |ws, msg|
   p msg
   ws.send('Up')
   p count
