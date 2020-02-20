cb8764c9cd213d61c4e74ce594a40dea117e8564
vgulaev <vgulaev@yandex.ru>
2020-02-20 16:22:43 +0500

    Battlecity beta

--- game_base_client.rb
+++ game_base_client.rb
@@ -1,14 +1,40 @@
 # require_relative "codenjoy/client"
 require "bundler/setup"
 require "codenjoy/client"
+require "codenjoy/games/battlecity/board"
+
+class YourSolver
+  # UP:   'up',                // you can move
+  # DOWN: 'down',
+  # LEFT: 'left',
+  # RIGHT:'right',
+  # ACT:  'act',               // fire
+  # STOP: ''                   // stay
+  def get_answer(board)
+
+    #######################################################################
+    #
+    #                     YOUR ALGORITHM HERE
+    #
+    #######################################################################
+
+    return 'act'
+  end
+
+end
 
 game = Codenjoy::Client::Game.new
+board = Codenjoy::Client::Games::Battlecity::Board.new
 
 url = "https://dojorena.io/codenjoy-contest/board/player/70xewv6o7ddy9yphm1u0?code=2603484461919438773&gameName=battlecity"
 count = 0
 
+
 game.play(url) do |ws, msg|
-  p msg
+  json = msg[6..-1].force_encoding('UTF-8')
+  board.process(json)
+  puts board.to_s
+
   ws.send('Up')
   p count
   count += 1
